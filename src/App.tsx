e2396749a55d025e371c07bb55799c2c0f106e6b
Lakshya11223 <lakshyadeepsharma61@gmail.com>
2026-01-31 15:22:26 +0530

    Store Created

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" >
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
